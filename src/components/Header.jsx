86a920f5ca78dfdc7b185254ccb58ffaa4170bd4
Kolten <kolten.pilar@gmail.com>
2023-09-17 21:21:05 -0400

    added header with image. began configuring routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import '../assets/images/WC.jpg'
+import React from "react";
+import WC from "../assets/images/WC.jpg";
+import cartIcon from "../assets/icons/cart.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <nav id='header' className='bg-black text-white'>
-        <div className="w-full mx-auto flex flex-wrap items-center justify-between mt-0 py-2 container">
-            <div className="logo-wrapper pl-4 flex items-center">
-                <img src={'/'} alt="logo" />
-            </div>
-            <div className="nav-menu-wrapper flex items-center justify-between space-x-10">
-                <div>Home</div>
-                <div>About</div>
-            </div>
-            <div className='flex items-center justify-center space-x-4'>
-                <div>Cart</div>
-                <div>Log In</div>
-                <div>Sign Up</div>
-            </div>
+    <nav id="header" className="bg-black text-white">
+      <div className="w-full mx-auto flex flex-wrap items-center justify-between mt-0 py-2 container">
+        <div className="logo-wrapper pl-4 flex items-center">
+          <Link
+            to={"/"}
+            className="toggleColor text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+          >
+            <img src={WC} alt="logo" className="h-40 w-40 object-cover" />
+          </Link>
         </div>
+        <div className="nav-menu-wrapper flex items-center justify-between space-x-10">
+          <Link to="/" className="text-xl">
+            Home
+          </Link>
+          <Link to="#" className="text-xl">
+            About
+          </Link>
+        </div>
+        <div className="flex items-center justify-center space-x-4">
+          <Link to="/cart">
+            <img src={cartIcon} alt="cart" />
+          </Link>
+          <Link>Log In</Link>
+          <Link>Sign Up</Link>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Header
+export default Header;
